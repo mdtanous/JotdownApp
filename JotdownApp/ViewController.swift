f30f6f61d70c9d60a38cbbaf9983bbe4efb26c48
Myles <myles.tanous@gmail.com>
2020-04-07 17:13:54 -0400

    Initial Commit

---
+++ JotdownApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JotdownApp
+//
+//  Created by Myles Tanous on 4/7/20.
+//  Copyright © 2020 Myles Tanous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
